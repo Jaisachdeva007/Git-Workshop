39621d2077984b0c9f97796cd54eabe51f35ee3c
jaisahdeva <jaisachdeva028@gmail.com>
2025-01-23 18:21:02 -0400

    added age

--- yourInro.java
+++ yourInro.java
@@ -1,11 +1,10 @@
 
-import java.util.*;
-
 public class yourInro {
     public static void main(String[]args){
-        Scanner jk = new Scanner(System.in);
+
 
         System.out.println("Hi I am jai") ;
+        System.out.println("I am 20 yrs old") ;
     }
 
 
