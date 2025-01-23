09f0a029d45cc17af8546f41f4529971a17cf66d
jaisahdeva <jaisachdeva028@gmail.com>
2025-01-23 00:38:11 -0400

    first

---
+++ yourInro.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class yourInro {
+    public static void main(String[]args){
+        Scanner jk = new Scanner(System.in);
+
+        System.out.println("Hi I am jai") ;
+    }
+
+
+
+}
