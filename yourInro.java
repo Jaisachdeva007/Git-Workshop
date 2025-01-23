54501d907a28cc78201dbced5276e3f1beffcb1b
Rachael Eruokwu <rc621290@dal.ca>
2025-01-23 18:50:40 -0400

    yippee

--- yourInro.java
+++ yourInro.java
@@ -1,12 +1,7 @@
-
 public class yourInro {
     public static void main(String[]args){
-
-
-        System.out.println("Hi I am jai") ;
-        System.out.println("I am 20 yrs old") ;
+        System.out.println("Hi I am Rachael") ;
+        System.out.println("I am 18 yrs old") ;
     }
-
-
 
 }
